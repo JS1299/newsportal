f064064d532bf460f16c28d5ab305f9c92c98835
Jurijs Sluncevs <jurashluncev@gmail.com>
2020-06-12 13:35:01 +0300

    Modified migrations and created models(Comment + Article)

--- database/migrations/2020_06_12_092151_create_articles_table.php
+++ database/migrations/2020_06_12_092151_create_articles_table.php
@@ -16,6 +16,10 @@ class CreateArticlesTable extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('title',50);
+            $table->string('brief_desc', 100);
+            $table->string('content', 10000);
+            $table->string('image', 300);
         });
     }
 
