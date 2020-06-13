d89161f7293000b7a2e7c6baac414b41ca4c862c
Jurijs Sluncevs <jurashluncev@gmail.com>
2020-06-13 12:21:10 +0300

    Modified migrations and added sign in/up functionality

--- resources/views/mainpage.blade.php
+++ resources/views/mainpage.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset('css/design.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 <body>
 
@@ -15,11 +17,31 @@
                 <li class="inline left">Politika</li>
                 <li class="inline left">Sports</li>
                 <li class="inline left">Kriminals</li>
-                @if (!Auth::guest())
-                    <li class="inline right">Welcome</li>
-                @else
-                    <li class="inline right">Sign In</li>
-                    <li class="inline right">Sign Up</li>
+                @if (Route::has('login'))
+                    @auth
+                        <li class="nav-item dropdown inline right">
+                            <a id="navbarDropdown" class="dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @else
+                        <li class="inline right"><a href="{{ route('login') }}">Login</a></li>
+                        @if (Route::has('register'))
+                            <li class="inline right"> <a href="{{ route('register') }}">Register</a></li>
+                        @endif
+                    @endauth
                 @endif
             </ul>
         </nav>
